dc697a02517ce48fafc6079efaa9e9f81746374e
Nataliia Tsirul <tcirulnatalya@gmail.com>
2023-10-13 14:00:22 +0100

    added functionality to single product

--- main.js
+++ main.js
@@ -13,3 +13,34 @@ if (close) {
     nav.classList.remove('active');
   });
 }
+
+const coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var content = this.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+}
+
+const mainImg = document.getElementById('mainImg');
+const smallImg = document.getElementsByClassName('sm-img');
+
+smallImg[0].onclick = () => {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = () => {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = () => {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = () => {
+  mainImg.src = smallImg[3].src;
+};
